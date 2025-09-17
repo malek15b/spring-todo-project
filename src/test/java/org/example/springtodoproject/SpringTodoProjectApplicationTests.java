b13e06f8bc6de8cbcda63fcd25a2e4b6c23b864c
malek.b. <malek.boulakbech@gmail.com>
2025-09-17 10:07:02 +0200

    Initial commit

---
+++ src/test/java/org/example/springtodoproject/SpringTodoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springtodoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
