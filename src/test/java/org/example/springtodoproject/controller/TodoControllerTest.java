5a8f1a12f2e89caec15c25e651553e8410893455
malek.b. <malek.boulakbech@gmail.com>
2025-09-17 13:27:55 +0200

    add test

--- src/test/java/org/example/springtodoproject/controller/TodoControllerTest.java
+++ src/test/java/org/example/springtodoproject/controller/TodoControllerTest.java
@@ -1,11 +1,15 @@
 package org.example.springtodoproject.controller;
 
+import org.example.springtodoproject.model.Status;
+import org.example.springtodoproject.model.Todo;
 import org.example.springtodoproject.repository.TodoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,11 +24,22 @@ class TodoControllerTest {
     private TodoRepository todoRepository;
 
     @Test
-    void updateTodo() {
-        //GIVEN
+    void getTodoById() throws Exception {
+        String todoId = "1";
+        Todo todo = new Todo(todoId,"Test", Status.OPEN);
+        todoRepository.save(todo);
 
-        //WHEN
+        String response = """
+                {
+                "id": "1",
+                "description": "Test",
+                "status": "OPEN"
+                }
+                """;
 
-        //THEN
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/todo/" + todoId))
+                //THEN
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(response));
     }
 }
